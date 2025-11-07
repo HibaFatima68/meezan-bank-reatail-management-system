a8640a6ac9c22a1aaf416cc4b7feee2b693d5e85
AbdullahKhalid03 <abdullahbk03@gmail.com>
2025-11-08 03:14:39 +0500

    chore: minor updates to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to app.
+          Edit <code>src/App.js</code> ronaldo.
         </p>
         <a
           className="App-link"
